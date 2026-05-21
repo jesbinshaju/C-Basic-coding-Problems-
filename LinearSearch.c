760fa2399a9887ea741dc381eef048d246d036d4
Jesbin Shaju <jesbinshaju30@gmail.com>
2026-05-21 21:48:51 +0530

    Fix input prompts and search logic in LinearSearch

--- LinearSearch.c
+++ LinearSearch.c
@@ -1,12 +1,14 @@
 #include<stdio.h>
+#include<stdlib.h>
 int main(){
     int arr[50],i,n,key;
     printf("No. of terms: ");scanf("%i",&n);
     for(i=0;i<n;i++){
-        printf("ENtr element in %i : ",i+1);scanf("%i",&arr[i]);
+        printf("Enter element in %i : ",i+1);scanf("%i",&arr[i]);
     }
-    printf("entr key : ");scanf("%i",&key);
+    printf("enter key to search : ");scanf("%i",&key);
     for(i=0;arr[i]!='\0';i++){
-        if(arr[i]==key)     printf("%i found in position %i",key,i+1);
+        if(arr[i]==key)     printf("%i found in position %i",key,i+1);break;
     }printf('\n');
+    return 0;
 }
